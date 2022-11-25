c01667b266f257f835f12a4a5c9c3fc06ae262c0
PhytoEpidemic <64293310+PhytoEpidemic@users.noreply.github.com>
2022-11-25 06:21:46 -0600

    allow for jpg

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -27,7 +27,7 @@ class PersonalizedBase(Dataset):
         for cl in classes:
             class_path = os.path.join(self.data_root, cl)
             for file_path in os.listdir(class_path):
-                if file_path.endswith(".png"):
+                if not file_path.endswith(".txt"):
                     image_path = os.path.join(class_path, file_path)
                     self.image_paths.append(image_path)
 
